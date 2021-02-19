2f160f2ac775edae78a8ab19850c5f6140e153e3
Zach Nielsen <zach@zachn.me>
2021-02-18 21:24:04 -0800

    Drawing and arrow keys

--- main.lua
+++ main.lua
@@ -1,12 +1,24 @@
 
 function love.load()
-
+    x = 100
+    y = 100
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("up") then
+        y = y - (50*dt)
+    end
+    if love.keyboard.isDown("down") then
+        y = y + (50*dt)
+    end
+    if love.keyboard.isDown("left") then
+        x = x - (50*dt)
+    end
+    if love.keyboard.isDown("right") then
+        x = x + (50*dt)
+    end
 end
 
 function love.draw()
-
+    love.graphics.rectangle("fill", x, y, 50, 80)
 end
